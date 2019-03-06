b997b350843fcd92311e004a5cd5c28da75ad585
Nihal <ni.xeron@yahoo.com>
2019-03-06 17:45:28 +0600

    profile added
    login done ,signup (conti.)

--- controllers/cognitio.js
+++ controllers/cognitio.js
@@ -6,4 +6,8 @@ router.get('/',function(req, res){
     res.render('cognitio/index');
 });
 
+
+router.get('/profile',function(req,res){
+    res.render('cognitio/profile'); 
+});
 module.exports = router;
--- controllers/login.js
+++ controllers/login.js
@@ -1,8 +1,36 @@
 var express=require('express');
+var userModel = require.main.require('./model/userModel');
 var router=express.Router();
 
 router.get('/',function(req,res){
-    res.render('login/index');
+     var data={err:null};
+    res.render('login/index',data);
 });
+
+
+router.post('/',function(req,res){
+   var user={ 
+       email:req.body.email,
+     password:req.body.pass
+    };
+    userModel.validate(user, function(result){
+			if(result != ""){
+				req.session.un = req.body.uname;
+				req.session.uid = result.id;
+                var varify=result.varified;
+                console.log(varify);
+                if(varify==0){
+                    var data={err:"Please varify your email address"};
+				res.render('login/index',data);
+                }else{
+				res.redirect('/');
+                }
+			}else{
+                var data={err:"Wrong Email or Password"};
+				res.render('login/index',data);
+			}		
+		});
+});
+
 
 module.exports = router;
